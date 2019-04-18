fca445a84c18e5e48d7aa28833eab18824876fbb
Korvin Szanto <Korvinszanto@gmail.com>
2019-04-18 16:32:28 -0700

    Prevent XSS in search

--- single_pages/search.php
+++ single_pages/search.php
@@ -1,11 +1,27 @@
+<?php defined('C5_EXECUTE') or die('Access Denied.');
+
+$url = Concrete\Core\Support\Facade\Url::to('/search');
+
+$defaultQuery = array_filter([
+    'filter' => $filter ?? null,
+    'keywords' => $qkeywords ?? null,
+    'ipp' => (int) $items_per_page ?? null,
+    'sortBy' => $sortBy ?? null
+]);
+$searchUrl = function($data = []) use ($url, $defaultQuery) {
+    $query = $url->getQuery();
+    $query->set($data + $defaultQuery);
+    return $url->setQuery($query);
+};
+?>
 
 <div class="container row top-search">
     <div class="col-xs-12 col-md-6 col-md-offset-3">
-        <form data-form="search" class="" method="get" action="<?=URL::to('/search')?>">
+        <form data-form="search" class="" method="get" action="<?= $searchUrl() ?>">
             <div class="search input-group">
                 <a href="#" class="fa fa-close search-clear"></a>
-                <input name="keywords" type="text" placeholder="Search in Brand Central." autocomplete="off" value="<?= $input_search ?>"/>
-                <input name="filter" type="hidden" value="<?= $filter ?>"/>
+                <input name="keywords" type="text" placeholder="Search in Brand Central." autocomplete="off" value="<?= h($input_search) ?>"/>
+                <input name="filter" type="hidden" value="<?= h($filter) ?>"/>
                 <div class="input-group-btn">
                     <button type="button" class="btn dropdown-toggle search-btn-filter" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="search-btn-label"><?= $filter_label?></span> <span class="caret"></span></button>
                     <ul class="dropdown-menu dropdown-menu-right" style="z-index: 1000">
@@ -25,7 +41,7 @@
 <div class="container search-results">
     <div class="row">
         <div class="col-xs-12 collection-container">
-            <?php if($count_results) { ?>
+            <?php if ($count_results) { ?>
                 <div class="row">
                     <div class="col-sm-8">
                         <h1><?= $input_search?> <small>Results <strong><?=t2('%s item', '%s items', $count_results)?>.</strong></small></h1>
@@ -35,9 +51,9 @@
                             <button class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sort Results <img src="<?=$view->getThemePath()?>/images/dropdown_filter.png"></button>
                             <ul class="dropdown-menu">
                                 <li><h4>Sort By</h4></li>
-                                <li><a href="<?= URL::to('/search')?>?filter=<?=$filter?>&amp;keywords=<?=h($qkeywords)?>&amp;ipp=<?=h($items_per_page)?>&amp;sortBy=recent"><i class="fa <?= $sortBy == 'recent' ? 'fa-dot-circle-o' : 'fa-circle-o' ?>"></i> Recent First</a></li>
-                                <li><a href="<?= URL::to('/search')?>?filter=<?=$filter?>&amp;keywords=<?=h($qkeywords)?>&amp;ipp=<?=h($items_per_page)?>&amp;sortBy=oldest"><i class="fa <?= $sortBy == 'oldest' ? 'fa-dot-circle-o' : 'fa-circle-o' ?>"></i> Oldest First</a></li>
-                                <li><a href="<?= URL::to('/search')?>?filter=<?=$filter?>&amp;keywords=<?=h($qkeywords)?>&amp;ipp=<?=h($items_per_page)?>&amp;sortBy=name"><i class="fa <?= $sortBy == 'name'? 'fa-dot-circle-o' : 'fa-circle-o' ?>"></i> Name</a></li>
+                                <li><a href="<?= $searchUrl(['sortBy' => 'recent']) ?>"><i class="fa <?= $sortBy === 'recent' ? 'fa-dot-circle-o' : 'fa-circle-o' ?>"></i> Recent First</a></li>
+                                <li><a href="<?= $searchUrl(['sortBy' => 'oldest']) ?>"><i class="fa <?= $sortBy === 'oldest' ? 'fa-dot-circle-o' : 'fa-circle-o' ?>"></i> Oldest First</a></li>
+                                <li><a href="<?= $searchUrl(['sortBy' => 'name']) ?>"><i class="fa <?= $sortBy == 'name'? 'fa-dot-circle-o' : 'fa-circle-o' ?>"></i> Name</a></li>
                             </ul>
                         </div>
                     </div>
@@ -56,7 +72,7 @@
 
                 <div class="row assets">
                     <?php foreach($search_assets as $asset) {
-                        $asset = new \Concrete5\AssetLibrary\Results\Formatter\Asset($asset);
+                        $asset = new \PortlandLabs\AssetLibrary\Results\Formatter\Asset($asset);
                         ?>
                         <div class="col-xs-12 col-sm-6 col-lg-4 thumbnail-container">
                             <a href="<?= \URL::to('/assets', $asset->getId()) ?>" class="thumbnail">
@@ -87,10 +103,10 @@
                             <div class="dropdown search-page-results" style="">
                                 <button data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="current-option"><?= h($items_per_page) ?> </span> <span class="caret"></span></button>
                                 <ul class="dropdown-menu">
-                                    <li><a href="<?= URL::to('/search')?>?filter=<?=$filter?>&amp;keywords=<?=h($qkeywords)?>&amp;sortBy=<?=h($sortBy)?>&amp;ipp=12">12</a></li>
-                                    <li><a href="<?= URL::to('/search')?>?filter=<?=$filter?>&amp;keywords=<?=h($qkeywords)?>&amp;sortBy=<?=h($sortBy)?>&amp;ipp=24">24</a></li>
-                                    <li><a href="<?= URL::to('/search')?>?filter=<?=$filter?>&amp;keywords=<?=h($qkeywords)?>&amp;sortBy=<?=h($sortBy)?>&amp;ipp=48">48</a></li>
-                                    <li><a href="<?= URL::to('/search')?>?filter=<?=$filter?>&amp;keywords=<?=h($qkeywords)?>&amp;sortBy=<?=h($sortBy)?>&amp;ipp=96">96</a></li>
+                                    <li><a href="<?= $searchUrl(['ipp' => 12]) ?>">12</a></li>
+                                    <li><a href="<?= $searchUrl(['ipp' => 24]) ?>">24</a></li>
+                                    <li><a href="<?= $searchUrl(['ipp' => 48]) ?>">48</a></li>
+                                    <li><a href="<?= $searchUrl(['ipp' => 96]) ?>">96</a></li>
                                 </ul>
                             </div>
                             Per Page
