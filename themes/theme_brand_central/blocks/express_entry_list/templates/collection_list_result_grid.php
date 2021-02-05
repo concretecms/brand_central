c408108d4d85c47b3661d7bc0a67ba1764d4e11f
Andrew Embler <andrew@concrete5.org>
2021-02-05 12:54:13 -0800

    updating templates'

--- themes/theme_brand_central/blocks/express_entry_list/templates/collection_list_result_grid.php
+++ themes/theme_brand_central/blocks/express_entry_list/templates/collection_list_result_grid.php
@@ -20,23 +20,32 @@ $itemsPerPageOptionUrl = function($itemsPerPage) {
 }
 ?>
 
+<?php
+if ($enableItemsPerPageSelection || $enablePagination) {
+?>
+
 <div class="row mt-2 mb-5">
-    <div class="col-sm-4">
-        <div class="dropdown">
-            <button data-toggle="dropdown" class="btn-round" aria-haspopup="true" aria-expanded="false"><?= h($itemsPerPageSelected) ?> <i class="fa fa-chevron-down"></i></button>
-            Per Page
-            <div class="dropdown-menu">
-                <a class="dropdown-item" href="<?= $itemsPerPageOptionUrl(12) ?>">12</a></li>
-                <a class="dropdown-item" href="<?= $itemsPerPageOptionUrl(24) ?>">24</a></li>
-                <a class="dropdown-item" href="<?= $itemsPerPageOptionUrl(48) ?>">48</a></li>
-                <a class="dropdown-item" href="<?= $itemsPerPageOptionUrl(96) ?>">96</a></li>
+    <?php if ($enableItemsPerPageSelection) { ?>
+        <div class="col-sm-4">
+            <div class="dropdown">
+                <button data-toggle="dropdown" class="btn-round" aria-haspopup="true" aria-expanded="false"><?= h($itemsPerPageSelected) ?> <i class="fa fa-chevron-down"></i></button>
+                Per Page
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="<?= $itemsPerPageOptionUrl(12) ?>">12</a></li>
+                    <a class="dropdown-item" href="<?= $itemsPerPageOptionUrl(24) ?>">24</a></li>
+                    <a class="dropdown-item" href="<?= $itemsPerPageOptionUrl(48) ?>">48</a></li>
+                    <a class="dropdown-item" href="<?= $itemsPerPageOptionUrl(96) ?>">96</a></li>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-sm-8 d-flex flex-row-reverse">
-        <?php if ($pagination) { ?>
-            <?=$pagination ?>
-        <?php } ?>
-    </div>
+    <?php } ?>
+    <?php if ($enablePagination) { ?>
+        <div class="col-sm-8 d-flex flex-row-reverse">
+            <?php if ($pagination) { ?>
+                <?=$pagination ?>
+            <?php } ?>
+        </div>
+    <?php } ?>
 </div>
+<?php } ?>
 
