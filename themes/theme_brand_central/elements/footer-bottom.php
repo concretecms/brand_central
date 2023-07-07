c95c4192fd64a4f701fbe7a4eb808da73ce69102
myq <myq@portlandlabs.com>
2023-07-06 17:24:49 -0700

    Fixes for PHP 8.2

--- themes/theme_brand_central/elements/footer-bottom.php
+++ themes/theme_brand_central/elements/footer-bottom.php
@@ -1,7 +1,7 @@
         </div>
         <?php View::element('footer_required'); ?>
 
-        <?php if (isset($lightboxApp)) { ?>
+<?php if (isset($lightboxApp)) { ?>
             <div id="lightbox-app"></div>
             <script src="<?= $view->getThemePath() ?>/js/lightbox.js"></script>
         <?php } ?>
